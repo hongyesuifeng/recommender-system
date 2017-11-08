fecd56b8751a79c5a1ad53e83b1cfacd4e357e12
hongyesuifeng <691644120@qq.com>
2017-11-08 17:25:04 +0800

    learn to create web

--- django_learn/mysite/polls/admin.py
+++ django_learn/mysite/polls/admin.py
@@ -2,6 +2,18 @@ from django.contrib import admin
 
 # Register your models here.
 
-from .models import Question 
+from .models import Question, Choice 
 
-admin.site.register(Question)
+class ChoiceInline(admin.StackedInline):
+    model = Choice
+    extra = 3
+
+
+class QuestionAdmin(admin.ModelAdmin):
+    fieldsets = [
+        (None,               {'fields': ['question_text']}),
+        ('Date information', {'fields': ['pub_date'], 'classes': ['collapse']}),
+    ]
+    inlines = [ChoiceInline]
+
+admin.site.register(Question, QuestionAdmin)
